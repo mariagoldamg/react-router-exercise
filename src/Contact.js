2da5cb15298f5cdf551d37baf655aeb925bc8768
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-28 17:03:29 +0400

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+function Contact(){
+    return(
+        <div>
+            <h1>contact us</h1>
+        </div>
+    )
+}
+export default Contact;
---
+++ src/Team.js
@@ -0,0 +1,8 @@
+function Team(){
+    return(
+        <div>
+            <h1>team</h1>
+        </div>
+    )
+}
+export default Team;
